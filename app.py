1dbe7edc3658f8556cd28988fe683dded80a6cbe
Daniel Lindo <dilindo28@gmail.com>
2017-11-02 01:41:19 -0700

    added tweet for hot albums

--- app.py
+++ app.py
@@ -18,31 +18,35 @@ def main():
         "EDM": ["EDM", "edm", "elctronic trap", "electro house", "house", "electro", "club", "dance", "electronic"],
         "Country": ["Country", "contemporary country", "country", "country road"],
     }
-    album_list = get_releases()
+    song_list = get_singles()
+    album_list = get_albums()
 
-    song_tweet(genre_list["Daily"], album_list)
-    song_tweet(genre_list["Pop"], album_list)
-    song_tweet(genre_list["Hip Hop"], album_list)
-    song_tweet(genre_list["EDM"], album_list)
-    song_tweet(genre_list["Country"], album_list)
+    song_tweet(genre_list["Daily"], song_list)
+    song_tweet(genre_list["Pop"], song_list)
+    song_tweet(genre_list["Hip Hop"], song_list)
+    song_tweet(genre_list["EDM"], song_list)
+    song_tweet(genre_list["Country"], song_list)
+    album_tweet(album_list)
 
 
-def song_tweet(genres, album_list):
-    song = album_list[random.randint(0, len(album_list) - 1)]
+def song_tweet(genres, song_list):
+    song = song_list[random.randint(0, len(song_list) - 1)]
     track_genres = find_genres(song[0])
     if song and any(x in track_genres for x in genres[1:]):
         api.update_status(status=genres[0] + " Pick: \n" + song[0] + " - " + song[1] + "\nListen here! " + song[2])
         return
     else:
-        song_tweet(genres, album_list)
+        song_tweet(genres, song_list)
 
 
-def album_tweet():
-    albums = sp.new_releases(limit=10)
-    for album in albums["albums"]["items"]:
-        if album["album_type"] == "album":
-            api.update_status(status="Album of the Week: " + album["artists"][0]["name"] + " - " + album["name"] + "\nListen here! " + album["href"])
-            return
+def album_tweet(album_list):
+    album = album_list[random.randint(0, len(album_list) - 1)]
+    album_genres = find_genres(album[0])
+    if album and any(x in album_genres for x in ["hip hop", "pop", "rap", "dwn trap", "dance"]):
+        api.update_status(status="Hot Album This Week: \n" + album[0] + " - " + album[1] + "\nListen here! " + album[2])
+    else:
+         album_tweet(album_list)
+
 #function searches for song & popularity
 def find_popularity(track, artist):
     popularity = 0
@@ -66,14 +70,33 @@ def find_genres(artist):
     return track_genres
 
 #fetches new releases
-def get_releases():
-    album_list = []
+def get_singles():
+    song_list = []
     releases = sp.new_releases()
     while releases:
             albums = releases['albums']
             for i, item in enumerate(albums['items']):
                 album_type = item["album_type"]
                 if album_type == "single":
+                    artist = item["artists"][0]["name"]
+                    album_name = item["name"]
+                    link = "https://open.spotify.com/album/" + item["id"]
+                    track = [artist, album_name, link]
+                    song_list.append(track)
+            if albums['next']:
+                releases = sp.next(albums)
+            else:
+                releases = None
+    return song_list
+
+def get_albums():
+    album_list = []
+    releases = sp.new_releases()
+    while releases:
+            albums = releases['albums']
+            for i, item in enumerate(albums['items']):
+                album_type = item["album_type"]
+                if album_type == "album":
                     artist = item["artists"][0]["name"]
                     album_name = item["name"]
                     link = "https://open.spotify.com/album/" + item["id"]
