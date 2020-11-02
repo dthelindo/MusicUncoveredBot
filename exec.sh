764b79b5e9533870d22b56fd0f7d81b2c82d0550
Dlindo28 <dilindo28@gmail.com>
2020-11-01 18:56:08 -0800

    monday shedule

--- exec.sh
+++ exec.sh
@@ -2,7 +2,7 @@ DAYOFWEEK=$(date +"%a");
 COMMAND="pipenv run musicuncovered.py";
 CM="pip -V"
 echo DAYOFWEEK: $DAYOFWEEK;
-if [ "$DAYOFWEEK" == "Mon" ]; 
+if [ $DAYOFWEEK == "Mon" ]; 
 then   
   pipenv run musicuncovered.py; 
 else
